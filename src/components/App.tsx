05f17e214346cead067fbf66b77b28002c7a3b78
Leo Robinovitch <leorobinovitch@gmail.com>
2021-04-05 16:29:35 -0700

    version 0.0.9

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Qty from "js-quantities"; // https://github.com/gentooboontoo/js-quantities/blob/master/src/quantities/definitions.js
 import Canvas from "./Canvas/Canvas";
+import * as hs from "hotstuff-network";
 
 export type Point = {
   xPos: number;
@@ -16,7 +17,38 @@ export type HotNode = {
   isBoundary: boolean;
 };
 
+const firstNode = hs.makeNode({
+  name: "first",
+  temperatureDegC: 10,
+  capacitanceJPerDegK: 10000,
+  powerGenW: 80,
+  isBoundary: false,
+});
+
+const secondNode = hs.makeNode({
+  name: "second",
+  temperatureDegC: 40,
+  capacitanceJPerDegK: 40000,
+  powerGenW: -10,
+  isBoundary: true,
+});
+
+const connection: hs.Connection = {
+  source: firstNode,
+  target: secondNode,
+  resistanceDegKPerW: 100,
+  kind: "bi",
+};
+
 export default function App() {
+  const results = hs.run({
+    nodes: [firstNode, secondNode],
+    connections: [connection],
+    timestepS: 0.01,
+    runTimeS: 0.03,
+  });
+  console.log(JSON.stringify(results, null, 2));
+
   const nodes: HotNode[] = [];
 
   function addNode(node: HotNode) {
