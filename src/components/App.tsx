c5799cee42f9d61722ea6fe685139a8710b6589f
Leo Robinovitch <leorobinovitch@gmail.com>
2021-04-07 12:37:18 -0700

    line chart

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
 import Qty from "js-quantities"; // https://github.com/gentooboontoo/js-quantities/blob/master/src/quantities/definitions.js
 import Canvas from "./Canvas/Canvas";
-import { run, makeNode, makeConnection, ModelOutput } from "hotstuff-network";
+import {
+  run,
+  makeNode,
+  makeConnection,
+  ModelOutput,
+  TempOutput,
+} from "hotstuff-network";
 import * as d3 from "d3";
 
 export type Point = {
@@ -23,7 +29,7 @@ function runModel() {
     name: "first",
     temperatureDegC: 10,
     capacitanceJPerDegK: 100,
-    powerGenW: 80,
+    powerGenW: 0,
     isBoundary: false,
   });
 
@@ -69,7 +75,7 @@ function runModel() {
     nodes: [firstNode, secondNode, thirdNode],
     connections: [conn12, conn23, conn31],
     timeStepS: 0.1,
-    totalTimeS: 1000,
+    totalTimeS: 100,
   });
   const end = performance.now();
 
@@ -77,6 +83,21 @@ function runModel() {
   console.log(`Model took ${end - start} ms`);
 
   return results;
+}
+
+function getTempRange(nodeTemps: TempOutput[]): number[] {
+  const range = [0, 0];
+  nodeTemps.forEach((temps) => {
+    const min = Math.min(...temps.tempDegC);
+    const max = Math.max(...temps.tempDegC);
+    if (min < range[0]) {
+      range[0] = min;
+    }
+    if (max > range[1]) {
+      range[1] = max;
+    }
+  });
+  return range;
 }
 
 export default function App() {
@@ -95,78 +116,126 @@ export default function App() {
     width: 600,
     height: 400,
     margin: {
-      left: 20,
-      right: 20,
-      top: 20,
-      bottom: 20,
+      left: 100,
+      right: 100,
+      top: 100,
+      bottom: 100,
     },
   };
 
+  // run model once on load
   useEffect(() => {
     const results = runModel();
     setResults(results);
   }, []);
 
+  // plot
   useEffect(() => {
     if (results === undefined) {
       return;
-    }
-
-    const data = results.temps[0].tempDegC;
-
-    if (!plot) {
+    } else if (!plot) {
       setPlot(d3.select(plotRef.current));
-    } else {
-      const [dataMin, dataMax] = d3.extent(data);
-      if (dataMin === undefined || dataMax === undefined) {
-        throw Error("data wack yo");
-      }
-
-      // add margin
-      const innerContent = plot
-        .attr(
-          "width",
-          plotParams.width + plotParams.margin.left + plotParams.margin.right
-        )
-        .attr(
-          "height",
-          plotParams.height + plotParams.margin.top + plotParams.margin.bottom
-        )
-        .append("g")
-        .attr(
-          "transform",
-          `translate(${plotParams.margin.left}, ${plotParams.margin.top})`
-        );
-
-      const scaleX = d3
-        .scaleLinear()
-        .domain([0, data.length * results.timeStepS])
-        .range([0, plotParams.width]);
-      const xAxis = d3.axisBottom(scaleX);
-
-      const scaleY = d3
-        .scaleLinear()
-        .domain([dataMin, dataMax])
-        .range([plotParams.height, 0]);
-      const yAxis = d3.axisLeft(scaleY);
-
-      // add axis
-      const xAxisGroup = innerContent
-        .append("g")
-        .attr("transform", `translate(0, ${plotParams.height})`)
-        .call(xAxis);
-      const yAxisGroup = innerContent.append("g").call(yAxis);
-
-      // plot data
-      innerContent
-        .selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("r", 1)
-        .attr("cy", (d) => scaleY(d))
-        .attr("cx", (_, idx) => scaleX(idx * results.timeStepS));
+      return;
     }
+
+    const [dataMin, dataMax] = getTempRange(results.temps);
+
+    // add margin
+    const innerContent = plot
+      .attr(
+        "width",
+        plotParams.width + plotParams.margin.left + plotParams.margin.right
+      )
+      .attr(
+        "height",
+        plotParams.height + plotParams.margin.top + plotParams.margin.bottom
+      )
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${plotParams.margin.left}, ${plotParams.margin.top})`
+      );
+
+    const scaleX = d3
+      .scaleLinear()
+      .domain([0, results.totalTimeS])
+      .range([0, plotParams.width]);
+    const xAxis = d3.axisBottom(scaleX);
+
+    const scaleY = d3
+      .scaleLinear()
+      .domain([dataMin, dataMax])
+      .range([plotParams.height, 0]);
+    const yAxis = d3.axisLeft(scaleY);
+
+    // xAxis
+    innerContent
+      .append("g")
+      .attr("transform", `translate(0, ${plotParams.height})`)
+      .call(xAxis)
+      .append("text")
+      .text("Time [seconds]")
+      .attr("fill", "black")
+      .attr("font-size", 14)
+      .attr("text-anchor", "middle")
+      .attr(
+        "x",
+        plotParams.margin.left +
+          (plotParams.width -
+            plotParams.margin.left -
+            plotParams.margin.right) /
+            2
+      )
+      .attr("y", 50); // Relative to the x axis.
+
+    // yAxis
+    innerContent
+      .append("g")
+      .call(yAxis)
+      .append("text")
+      .text("Temperature [degC]")
+      .attr("fill", "black")
+      .attr("font-size", 14)
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .attr(
+        "x",
+        -(
+          plotParams.margin.top +
+          (plotParams.height -
+            plotParams.margin.top -
+            plotParams.margin.bottom) /
+            2
+        )
+      )
+      .attr("y", -50); // Relative to the y axis.
+
+    const lineGen = d3
+      .line()
+      .x((d) => scaleX(d[0]))
+      .y((d) => scaleY(d[1]))
+      .curve(d3.curveMonotoneX);
+
+    // plot data
+    results.temps.map((temps) => {
+      const data: [number, number][] = temps.tempDegC.map((val, idx) => [
+        idx * results.timeStepS,
+        val,
+      ]);
+      const lineData = lineGen(data);
+      console.log(data);
+      console.log(lineData);
+      if (!!lineData) {
+        innerContent
+          .append("path")
+          .attr("fill", "none")
+          .attr("d", lineData)
+          .attr("stroke", "steelblue");
+        // .attr("stroke-linejoin", "round")
+        // .attr("stroke-linecap", "round")
+        // .attr("stroke-width", 1.5);
+      }
+    });
   }, [plot, results]);
 
   return (
