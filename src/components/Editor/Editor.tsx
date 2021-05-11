f53e07e5c3572b31a3772fadd3740106ba02d14c
Leo Robinovitch <leorobinovitch@gmail.com>
2021-05-11 14:04:05 -0700

    tabs for tables

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import styled from "styled-components";
 import { AppNode } from "../App";
 import NodeTable from "./EditableTable/NodeTable";
+import Tabs from "../Tabs/Tabs";
 
 const StyledEditor = styled.div<{ width: number; height: number }>`
   height: ${(props) => props.height}px;
@@ -21,12 +22,20 @@ type EditorProps = {
 };
 
 export default function Editor(props: EditorProps) {
+  const nodeTable = (
+    <NodeTable
+      rows={props.nodes}
+      onUpdateRow={(node: AppNode) => props.updateNodes([node])}
+      onDeleteRow={(node: AppNode) => props.deleteNodes([node.id])}
+    />
+  );
   return (
     <StyledEditor width={props.width} height={props.height}>
-      <NodeTable
-        rows={props.nodes}
-        onUpdateRow={(node: AppNode) => props.updateNodes([node])}
-        onDeleteRow={(node: AppNode) => props.deleteNodes([node.id])}
+      <Tabs
+        tabs={[
+          { text: "Nodes", component: nodeTable, width: 0.5 },
+          { text: "Connections", component: <h1>hi</h1>, width: 0.5 },
+        ]}
       />
     </StyledEditor>
   );
---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import styled from "styled-components/macro";
+
+const StyledTabs = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+  border-bottom: 1px solid black;
+`;
+
+const StyledTab = styled.div`
+  cursor: pointer;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  background: lightgray;
+  /* background: rgb(85, 9, 121);
+  background: linear-gradient(
+    90deg,
+    rgba(85, 9, 121, 1) 0%,
+    rgba(255, 0, 230, 1) 49%,
+    rgba(46, 228, 233, 1) 100%
+  ); */
+  border-radius: 5px 5px 0 0;
+  border: 1px solid black;
+
+  &:hover {
+    opacity: 0.75;
+  }
+`;
+
+const StyledTabText = styled.div`
+  font-size: 1.1em;
+  padding: 0.2em 0.5em;
+
+  &.active {
+    font-weight: bold;
+  }
+`;
+
+const StyledInnerContent = styled.div<{ topLeftRounded: boolean }>`
+  border-radius: ${({ topLeftRounded }) =>
+    `${topLeftRounded ? "20px" : "0"} 20px 20px 20px`};
+  background: white;
+`;
+
+type Tab = {
+  text: string;
+  component: React.ReactElement;
+  width: number; // between 0 and 1
+};
+
+export type TabsProps = {
+  tabs: Tab[];
+};
+
+export default function Tabs(props: TabsProps): React.ReactElement {
+  const [activeIdx, setActiveIdx] = useState(0);
+
+  return (
+    <div>
+      {props.tabs.length > 1 && (
+        <StyledTabs>
+          {props.tabs.map((tab, idx) => {
+            return (
+              <StyledTab
+                key={tab.text}
+                onClick={() => setActiveIdx(idx)}
+                style={{ width: `${tab.width * 100}%` }}
+              >
+                <StyledTabText className={idx === activeIdx ? "active" : ""}>
+                  {tab.text}
+                </StyledTabText>
+              </StyledTab>
+            );
+          })}
+        </StyledTabs>
+      )}
+
+      <StyledInnerContent topLeftRounded={props.tabs.length <= 1}>
+        {props.tabs.filter((_, idx) => idx === activeIdx)[0].component}
+      </StyledInnerContent>
+    </div>
+  );
+}
