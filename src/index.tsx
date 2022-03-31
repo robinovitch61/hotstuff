5aa6e283c419138f42e66a0df781cf8339ff08be
Leo Robinovitch <leorobinovitch@gmail.com>
2022-03-30 20:39:39 -0700

    Add plausible

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { forceShowScrollbars } from "./utils/scrollBar";
+import Plausible from "plausible-tracker";
+
+const plausible = Plausible({
+  domain: "thermalmodel.com",
+  apiHost: "https://plausible.theleo.zone",
+});
+
+plausible.trackPageview();
 
 forceShowScrollbars();
 
