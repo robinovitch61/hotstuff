c8d6c5aa0d2a1e2f036976b3a0d819fd868dcb2c
Leo Robinovitch <leorobinovitch@gmail.com>
2026-04-13 18:10:14 -0700

    remove plausible, pass through referrer

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { forceShowScrollbars } from "./utils/scrollBar";
-import Plausible from "plausible-tracker";
-
-const plausible = Plausible({
-  domain: "thermalmodel.com",
-  apiHost: "https://plausible.theleo.zone",
-});
-
-plausible.enableAutoPageviews();
 
 forceShowScrollbars();
 
